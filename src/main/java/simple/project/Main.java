70ef5832f5074db67c5d8a550f0cafd20705722f
Sylvain Gournay <sylvain.gournay@logica.com>
2012-12-27 19:45:34 +0100

    changing port setting

--- src/main/java/simple/project/Main.java
+++ src/main/java/simple/project/Main.java
@@ -11,16 +11,25 @@ import com.sun.net.httpserver.HttpServer;
 public class Main {
 
 	public static void main(String... args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
+        String port = System.getenv("PORT");
+        if (port == null) {
+        	System.out.println("Environment variable not set, reading java property...");
+        	port = System.getProperty("PORT");
+        }
+        if (port == null) {
+        	System.out.println("Java property not set... error...");
+        	throw new IOException("Port not defined for the server");
+        }
+		HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(port)), 0);
         server.createContext("/", new HttpHandler() {
         	public void handle(HttpExchange t) throws IOException {
         		t.sendResponseHeaders(200, 0);
                 OutputStream out = t.getResponseBody();
-                out.write("hello world".getBytes());
+                out.write("hello".getBytes());
                 out.close();
         	}
         });
         server.start();
-        System.err.println("Server started");
+        System.out.println("Server started on port " + port);
 	}
 }
